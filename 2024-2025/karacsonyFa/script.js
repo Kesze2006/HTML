9bb3df49f52904884094a14f5c8d28a4d8306004
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-12-18 12:32:25 +0100

    sad

---
+++ 2024-2025/karacsonyFa/script.js
@@ -0,0 +1,6 @@
+function csinald()
+{
+    let haromszog =  document.createElement("div")
+    haromszog.setAttribute("class","triangle")
+    document.body.insertBefore(haromszog,document.body.firstChild)
+}
