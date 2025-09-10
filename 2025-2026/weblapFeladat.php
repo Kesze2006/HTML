a5a27ee29ddf922868c005633b4014dbbdf90eee
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-09-10 20:24:04 +0200

    asd

--- 2025-2026/weblapFeladat.php
+++ 2025-2026/weblapFeladat.php
@@ -1,61 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Weblap Feladat</title>
 </head>
+
 <body>
-    <div class="conatiner">
-        <div class="d-flex justify-content-center">
-            <p class="h1 ">Oldalcíme</p>
-        </div>
-        <div class="row">
-            <div class="col-3 d-inline-block">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>   
-            </div>
-            <div class="col-9 d-inline-block">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Features</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Pricing</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+
+<div class="container-lg">
+    <div class="row">
+        <div class="col-12 text-center">
+            <div class="display-1">Az oldal címe</div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-4 p-2">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-8 p-2">
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">Navbar</span>
+                </div>
+            </nav>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit. Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4 justify-content-center">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-8">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit. Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <footer class="bg-dark text-white text-center">
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a.</p>
+            </footer>
+        </div>
+    </div>
+</div>
 
 
 
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
