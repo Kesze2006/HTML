a1e69eac37e00706f537d24cb4ea979b10d71403
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-09-11 11:23:11 +0200

    asd

---
+++ 2025-2026/feladatok/adatok.php
@@ -0,0 +1,40 @@
+<?php
+    $adatok = [
+        "cim"=>[
+            "Budapest-Debrecen gyorsforgalmi útszakasz",
+            "két katica",
+            "három kiskacsa",
+            "négy porszívó",
+            "öt póniló",
+            "hat pillangó",
+            "hét tudjha a g*ci",
+            "nyolc hinta",
+            "kilnec 11",
+            "tíz kiskutya",
+        ],
+        "kartya"=>[
+            "cim"=>[
+                "Lajos",
+                "Bélabá",
+                "Kriszta",
+                "Anikó",
+                "Sándor",
+            ],
+            "szoveg"=>[
+                "Lajos nagyon szereti a paprikát",
+                "Béla nem szereti Magdit",
+                "ghfghnfghfgihfg",
+                "Nagyon szép az ég",
+                "Alma Alma Alma Alma Alma",
+            ],
+            "link"=>[
+                "#Lajos",
+                "#Bélabá",
+                "#Kriszta",
+                "#Anikó",
+                "#Sándor",
+            ]
+        ]
+    ];
+
+?>
--- 2025-2026/feladatok/weblapFeladat.php
+++ 2025-2026/feladatok/weblapFeladat.php
@@ -1,3 +1,12 @@
+<?php
+    include("fugvenyek.php");
+    
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +28,7 @@
     </div>
     <div class="row">
         <div class="col-sm-4 p-2">
-            <div class="card h-100">
+            <div class="card w-100" >
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,11 +42,12 @@
                     <span class="navbar-brand mb-0 h1">Navbar</span>
                 </div>
             </nav>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit. Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit.<img src="images.jpg" class=" rounded img-fluid float-end">Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+            
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-4 justify-content-center">
+        <div class="col-sm-4 justify-content-center p-2">
             <div class="card h-100">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
@@ -46,23 +56,22 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-8">
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit. Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+        <div class="col-sm-8 p-2">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<img src="images2.jpg" class="rounded img-fluid float-start"> Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit. Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12">
+        <div class="col-sm-12 p-2">
             <footer class="bg-dark text-white text-center">
                 <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a.</p>
             </footer>
         </div>
     </div>
 </div>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
+
 </body>
 
 </html>
