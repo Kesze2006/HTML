54071fe579c418ece2a6a5ab356c1b3ba4b254a9
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-09-17 12:34:19 +0200

    sad

--- 2025-2026/titkos2/index.php
+++ 2025-2026/titkos2/index.php
@@ -6,8 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Titkos oldal</h1>
-    <a href="http://logout@localhost/HTML/2025-2026/titkos/">Kilépés</a>
+    <h1>Titkos oldal Part 2</h1>
+    <a href="http://logout@localhost/HTML/2025-2026/titkos2/">Kilépés</a>
     <!--Chromban kell egy ENTER az url elé második bejelentkezéskor mert az a szar eltárolja a felhsználót-->
 </body>
 </html>
