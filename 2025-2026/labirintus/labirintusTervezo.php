490b814a92742bf826cbe0e4c449b9fe7f3d3127
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-10-03 12:23:10 +0200

    asd

--- 2025-2026/labirintus/labirintusTervezo.php
+++ 2025-2026/labirintus/labirintusTervezo.php
@@ -1,4 +1,9 @@
 <?php
+    function d($elem)
+    {
+        echo "<pre>".var_dump($elem)."</pre>";
+    }
+
     function tablaKeszit()
     {
         global $config;
@@ -9,7 +14,12 @@
             $vissza .= "<tr>";
             for($cella=0;$cella < $config["oszlop"];$cella++)
             {
-                $vissza .= "<td>" . $cella . "</td>";
+                $gombSzin = "";
+                if(isset($_SESSION["labirintus"][$sor][$cella]) && $_SESSION["labirintus"][$sor][$cella])
+                {
+                    $gombSzin= " fal";
+                }
+                $vissza .= '<td><input class="'.$gombSzin.'" type="submit" value="" name="gomb-'.$sor."-".$cella.'"></td>';
             }
             $vissza .= "</tr>";
         }
@@ -19,9 +29,38 @@
 
 
 
-    //session_start();
+    session_start();
     $config["oszlop"]=10;
     $config["sor"]=10;
+
+
+    if(isset($_GET))
+    {
+        $t = array_keys($_GET);
+        foreach($t as $index)
+        {
+            if(str_starts_with($index,"gomb-"))
+            {
+                $darabok = explode("-",$index);
+                if(sizeof($darabok) == 3)
+                {
+                    if(isset($_SESSION["labirintus"][$darabok[1]][$darabok[2]]) && $_SESSION["labirintus"][$darabok[1]][$darabok[2]])
+                    {
+                        $_SESSION["labirintus"][$darabok[1]][$darabok[2]] = false;
+                    }
+                    else{
+                        $_SESSION["labirintus"][$darabok[1]][$darabok[2]]=true;
+                    }
+                    
+                }
+            }
+        }
+        if(isset($_GET["save"]))
+        {
+            $_SESSION["mentettLabirintusok"][$_GET["save"]] = $_SESSION["labirintus"];
+        }
+    }
+    d($_SESSION["mentettLabirintusok"]);
     $tablaKesz = tablaKeszit();
 ?>
 
@@ -41,10 +80,22 @@
             width: 25px;
             height: 25px;
         }
+        td input[type=submit]
+        {
+            height: 100%;
+            width: 100%;
+        }
+        input.fal
+        {
+            background-color: red;
+        }
     </style>
 </head>
 <body>
     <h1>Labirintus szerkesztő</h1>
-    <?php echo $tablaKesz;?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+        <?php echo $tablaKesz;?>
+        <button type="submit" name="save" value="1">Mentés</button>        
+    </form>
 </body>
 </html>
